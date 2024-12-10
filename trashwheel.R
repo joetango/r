09859a429e549308895b2ff38dd32f0a2de79c59
Joe <joetdickerson@gmail.com>
2024-12-09 21:20:42 -0500

    plastic bottles

--- trashwheel.R
+++ trashwheel.R
@@ -2,11 +2,12 @@ require(tidyverse)
 
 trashwheel <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2024/2024-03-05/trashwheel.csv')
 
-trashwheel %>% 
+tw <- trashwheel %>% 
   mutate(Date = as_date(Date, format=c("%m/%d/%Y", "%m/%d/%y"))) %>% 
   group_by(Name, Date) %>% 
-  summarize(tot_weight = sum(Weight)) %>% 
+  summarize(Weight = sum(Weight),
+            Volume = sum(Volume), PlasticBottles = sum(PlasticBottles)) %>% 
   arrange(Date)
 
 
-head(trashwheel)
+tw
