b8cf0cf0f038dd8866fca713cad6a177f99e805c
Joe <joetdickerson@gmail.com>
2025-11-26 18:58:30 -0500

    changes

--- 6097-final-proj.R
+++ 6097-final-proj.R
@@ -12,6 +12,8 @@ data <- data %>%
     Affects_Academic_Performance == "No", 0, 1
   ))
 
+attach(data)
+
 g1 <- data %>% 
   ggplot(aes(y = Avg_Daily_Usage_Hours, x = Addicted_Score)) +
     geom_point() +
@@ -32,16 +34,42 @@ data %>%
 data %>% 
   ggplot(aes(x = Most_Used_Platform, y = Avg_Daily_Usage_Hours)) +
   geom_boxplot()
+
+data %>% 
+  ggplot(aes(x = Most_Used_Platform, color = Gender)) +
+  geom_bar()
+
   
 
 grid.arrange(g1, g2, nrow = 1)
 
-glmod <- glm(Avg_Daily_Usage_Hours ~ Age + GenderNumeric +
+## Linear Model
+
+lmod <- glm(Avg_Daily_Usage_Hours ~ Age + GenderNumeric +
                Avg_Daily_Usage_Hours + 
                Affects_Academic_Performance_Numeric +
                Sleep_Hours_Per_Night +
                Mental_Health_Score +
                Addicted_Score,
-               data = data)
+               data = data, family = "gaussian")
+
+summary(lmod)
+
+## Logistic Model
+
+glmod <- glm(Affects_Academic_Performance_Numeric ~ 
+               Avg_Daily_Usage_Hours +
+               Sleep_Hours_Per_Night,
+             data = data, family = "binomial")
 
 summary(glmod)
+
+## Random Forest
+
+rf.model <- randomForest(Mental_Health_Score ~ Avg_Daily_Usage_Hours +
+                        Age + GenderNumeric + 
+                        Affects_Academic_Performance_Numeric +
+                        Sleep_Hours_Per_Night + Addicted_Score, 
+                         data = data)
+rf.model
+
