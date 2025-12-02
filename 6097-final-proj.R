04fe32c29897509341339d73566b23c0636c008d
Joe <joetdickerson@gmail.com>
2025-12-01 20:19:30 -0500

    a lot

--- 6097-final-proj.R
+++ 6097-final-proj.R
@@ -6,71 +6,64 @@ library(tree)
 library(e1071)
 
 
+## Read data
+
 data <- read.csv("social-media.csv")
 
+## Data cleaning
+
 data <- data %>% 
-  mutate(GenderNumeric = ifelse(Gender == "Male", 1, 0)) %>% 
-  mutate(Affects_Academic_Performance_Numeric = ifelse(
-    Affects_Academic_Performance == "No", 0, 1)) %>% 
-    mutate(Mental_Health_Score_Class = ifelse(
-      Mental_Health_Score <= 5, 0, 1
-    ))
+  mutate(Gender = as.factor(Gender),
+         Affects_Academic_Performance = 
+           as.factor(Affects_Academic_Performance)) %>%
+  select(-Country,
+         -Most_Used_Platform,
+         -Relationship_Status,
+         -Conflicts_Over_Social_Media,
+         -Addicted_Score,
+         -Student_ID,
+         -Academic_Level) %>% 
+  slice(-705) 
+  
+AAP.numeric <- ifelse(data$Affects_Academic_Performance == "No", 0, 1)
 
-data <- data[, -5]
+## Exploratory plots
 
-attach(data)
-
-g1 <- data %>% 
-  ggplot(aes(y = Avg_Daily_Usage_Hours, x = Addicted_Score)) +
-    geom_point() +
-    geom_smooth(fill = NA) +
-    theme_minimal()
-
-g2 <- data %>% 
+data %>% 
   ggplot(aes(y = Avg_Daily_Usage_Hours, x = Mental_Health_Score)) +
   geom_point() +
-  geom_smooth(fill = NA) +
-  theme_minimal() +
-  theme(axis.title.y = element_blank())
+  geom_smooth(fill = NA, color = "red") +
+  labs(title = "Average Daily Social Media Usage vs. Mental Health Score",
+       y = "Average Daily Use (Hours)",
+       x = "Mental Health Score") +
+  theme_light() 
 
-data %>% 
-  ggplot(aes(x = Gender, y = Avg_Daily_Usage_Hours)) +
-  geom_boxplot()
+data %>%
+  ggplot(aes(y = Avg_Daily_Usage_Hours, x = Affects_Academic_Performance)) +
+  geom_boxplot() +
+  labs(title = "Average Daily Social Media Usage and Academic Performance",
+       y = "Average Daily Use (Hours)",
+       x = "Affects Academic Performance") +
+  theme_light()
 
-data %>% 
-  ggplot(aes(x = Most_Used_Platform, y = Avg_Daily_Usage_Hours)) +
-  geom_boxplot()
+## Linear model
 
-data %>% 
-  ggplot(aes(x = Most_Used_Platform, color = Gender)) +
-  geom_bar()
-
-  
-
-grid.arrange(g1, g2, nrow = 1)
-
-## Linear Model
-
-lmod <- glm(Mental_Health_Score ~ Age + GenderNumeric +
-               Avg_Daily_Usage_Hours + 
-               Affects_Academic_Performance_Numeric +
-               Sleep_Hours_Per_Night +
-               Avg_Daily_Usage_Hours +
-               Addicted_Score,
-               data = data, family = "gaussian")
-
+lmod <- lm(Mental_Health_Score ~ ., data = data)
 summary(lmod)
+
 
 ## Logistic Model
 
-glmod <- glm(Mental_Health_Score ~ 
-               Avg_Daily_Usage_Hours +
+glmod <- glm(Affects_Academic_Performance ~ Age + 
+               Gender + 
+               Avg_Daily_Usage_Hours + 
                Sleep_Hours_Per_Night,
              data = data, family = "binomial")
 
 summary(glmod)
 
-## Random Forest
+
+## Random Forest  
 
 set.seed(1)
 
@@ -78,39 +71,37 @@ seq <- sample(1:nrow(data), (nrow(data)/2))
 train <- data[seq, ]
 test <- data[-seq, ]
 
-rf.model <- randomForest(Mental_Health_Score ~ Avg_Daily_Usage_Hours +
-                        Age + GenderNumeric + 
-                        Affects_Academic_Performance_Numeric +
-                        Sleep_Hours_Per_Night + Addicted_Score, 
-                         data = train)
+rf.model <- randomForest(Mental_Health_Score ~ ., data = train)
 
 yhat.rf <- predict(rf.model, newdata = test)
-mean((yhat.rf - test[,"Mental_Health_Score"])^2) ## test mse 0.0997
+mean((yhat.rf - test[,"Mental_Health_Score"])^2) ## test mse 0.1022
                                                   ## similar to rf.model summary
 
 plot(yhat.rf, test[,"Mental_Health_Score"])
 abline(0, 1)
 
-## SVM
+## SVM Fit
 
-svm.fit <- svm(Mental_Health_Score_Class ~ ., data = train, kernel = "linear",
-              cost = 0.01, scale = FALSE)
-
-summary(svm.fit)
-
-tune.out <- e1071::tune(svm, Mental_Health_Score_Class ~ .,
+tune.out <- e1071::tune(svm, Affects_Academic_Performance ~ .,
                         data = train, kernel = "linear",
                         ranges = list(cost = c(.01, 1, 2, 4, 6, 8, 10)))
 summary(tune.out)
 plot(tune.out)  ## cost = 1
 
-svm.fit <- svm(Mental_Health_Score_Class ~ ., data = train, kernel = "linear",
+svm.fit <- svm(Affects_Academic_Performance ~ .,
+               data = train, kernel = "linear",
                cost = 1, scale = FALSE)
 
 summary(svm.fit)
 
+yhat.train.svm <- predict(svm.fit, newdata = train, type = "class")
+mean(yhat.train.svm != 
+       train$Affects_Academic_Performance) ## training error 0.0540
+
+
 yhat.svm <- predict(svm.fit, newdata = test, type = "class")
-table(yhat.svm, test$Mental_Health_Score_Class)
+mean(yhat.svm != test$Affects_Academic_Performance) ## test error 0.0369
 
-
+table(yhat.svm, test$Affects_Academic_Performance) ##checking test error
+(13) / (209 + 126 + 17) ## 0.0369 
 
