ce678d616affa6d75a3a44d71f6adf449a8d32d5
Joe <joetdickerson@gmail.com>
2026-01-19 17:52:40 -0500

    tidied up clm script for shinyapp, started shinyapp

---
+++ clm/app.R
@@ -0,0 +1,81 @@
+################
+### Packages ###
+################
+
+library(shiny)
+library(ggplot2)
+
+###########################
+### Initialize Function ###
+###########################
+
+fnc <- function(n){
+  vec <- numeric(n)
+  
+  for(i in 1:n){
+    vec[i] <- sum(sample(1:6, 6, replace = TRUE))
+  }
+  return(as.data.frame(vec))
+}
+
+##########
+### UI ###
+##########
+
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Central Limit Theorem Illustration"),
+
+    # Sidebar 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("rolls",
+                        "Number of Rolls:",
+                        choices = c(
+                          10, 100, 250, 1000, 10000
+                        )),
+            actionButton(
+              "runplot", "Generate Plot"
+            )
+        ),
+
+        # Plot
+        mainPanel(
+           plotOutput("plot")
+        )
+    )
+)
+
+##############
+### Server ###
+##############
+
+server <- function(input, output) {
+  
+  
+  
+  data <- eventReactive(input$runplot, {
+      fnc(as.numeric(input$rolls))
+  })
+
+    output$plot <- renderPlot({
+      ggplot(data = data(), aes(x = vec)) +
+        geom_bar(fill = "lightblue", size = .5) +
+        stat_density(geom = "line", aes(y = ..density.. * n), 
+                     color = "orange", linewidth = .8) +
+        scale_x_continuous(breaks = seq(min(data()$vec), max(data()$vec), by = 1)) +
+        theme_minimal() +
+        labs(title = paste("6d6 Roll Frequency With",
+                           length(data()$vec), "Rolls"),
+             x = "Roll Value",
+             y = "Number of Rolls") +
+        theme(plot.title = element_text(hjust = .5))
+    })
+}
+
+###########
+### Run ###
+###########
+
+shinyApp(ui = ui, server = server)
